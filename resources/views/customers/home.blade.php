45b23fcc5ce2ffe3ae23832043431cee13a95209
Feek18 <bintangfikri31@gmail.com>
2024-01-14 23:55:55 +0800

    update

--- resources/views/customers/home.blade.php
+++ resources/views/customers/home.blade.php
@@ -92,7 +92,7 @@
 
   <div id="default-carousel" class="relative w-full" data-carousel="slide">
     <!-- Carousel wrapper -->
-    <div class="relative h-56 overflow-hidden md:h-96">
+    <div class="relative lg:h-[650px] top-4 overflow-hidden md:h-96 h-56 -top-3">
       <!-- Item 1 -->
       <div class="hidden duration-700 ease-in-out" data-carousel-item>
         <img src="img/header1.png" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
