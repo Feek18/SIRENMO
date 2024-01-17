9fcbf1374a1001e68f0ef90fe0e2616f7963da38
Feek18 <bintangfikri31@gmail.com>
2024-01-18 03:24:33 +0800

    u

--- resources/views/customers/home.blade.php
+++ resources/views/customers/home.blade.php
@@ -89,140 +89,20 @@
     </div>
   </nav>
 
-  <!-- Banner -->
-  <div id="default-carousel" class="relative w-full" data-carousel="slide">
-    <!-- Carousel wrapper -->
-    <div class="relative lg:h-[650px] overflow-hidden lg:h-96 h-56 top-2">
-      <!-- Item 1 -->
-      <div class="hidden duration-700 ease-in-out" data-carousel-item>
-        <img src="img/header1.png" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-          alt="...">
-      </div>
-      <!-- Item 2 -->
-      <div class="hidden duration-700 ease-in-out" data-carousel-item>
-        <img src="img/header1.png" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-          alt="...">
+  <!-- Banner Information -->
+  <section >
+    <div class="">
+      <div>
+        <h1></h1>
       </div>
     </div>
-    <!-- Slider indicators -->
-    <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-      <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1"
-        data-carousel-slide-to="0"></button>
-      <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2"
-        data-carousel-slide-to="1"></button>
-      <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3"
-        data-carousel-slide-to="2"></button>
-      <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4"
-        data-carousel-slide-to="3"></button>
-      <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5"
-        data-carousel-slide-to="4"></button>
-    </div>
-    <!-- Slider controls -->
-    <button type="button"
-      class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-      data-carousel-prev>
-      <span
-        class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-        <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-            d="M5 1 1 5l4 4" />
-        </svg>
-        <span class="sr-only">Previous</span>
-      </span>
-    </button>
-    <button type="button"
-      class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-      data-carousel-next>
-      <span
-        class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-        <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-            d="m1 9 4-4-4-4" />
-        </svg>
-        <span class="sr-only">Next</span>
-      </span>
-    </button>
-  </div>
+  </section>
 
   <!-- Card Product -->
   <section>
     <div>
       <div>
         
-      </div>
-    </div>
-  </section>
-
-  <!-- Testimonials -->
-  <section class="testi mt-8">
-    <div class="heading">
-      <span class="text-color">Testimonials</span>
-      <h1>What Our Customers Say</h1>
-    </div>
-    <div class="testi-container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-      <div class="box bg-white p-6 rounded-lg">
-        <div class="stars">
-          <i class="fa fa-solid fa-star"></i>
-          <i class="fa fa-solid fa-star"></i>
-          <i class="fa fa-solid fa-star"></i>
-          <i class="fa fa-solid fa-star"></i>
-          <i class="fa fa-solid fa-star-half"></i>
-        </div>
-        <p class="pt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio praesentium delectus porro
-          voluptate ut itaque ducimus vel corporis minus dolores!</p>
-        <div class="flex items-center mt-2">
-          <div class="testi-img">
-            <img class="h-12 w-12 object-cover rounded-full mr-4" src="img/testi1.png" alt="Morgan Steve">
-          </div>
-          <div>
-            <h2 class="font-bold">Morgan Steve</h2>
-            <span>Traveller</span>
-          </div>
-        </div>
-      </div>
-
-      <div class="box bg-white p-6 rounded-lg">
-        <div class="stars">
-          <i class="fa fa-solid fa-star"></i>
-          <i class="fa fa-solid fa-star"></i>
-          <i class="fa fa-solid fa-star"></i>
-          <i class="fa fa-solid fa-star"></i>
-          <i class="fa fa-solid fa-star-half"></i>
-        </div>
-        <p class="pt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio praesentium delectus porro
-          voluptate ut itaque ducimus vel corporis minus dolores!</p>
-        <div class="flex items-center mt-2">
-          <div class="testi-img">
-            <img class="h-12 w-12 object-cover rounded-full mr-4" src="img/testi2.png" alt="Morgan Steve">
-          </div>
-          <div>
-            <h2 class="font-bold">Jessica Lawrence</h2>
-            <span>Food Vlogger</span>
-          </div>
-        </div>
-      </div>
-
-      <div class="box bg-white p-6 rounded-lg">
-        <div class="stars">
-          <i class="fa fa-solid fa-star"></i>
-          <i class="fa fa-solid fa-star"></i>
-          <i class="fa fa-solid fa-star"></i>
-          <i class="fa fa-solid fa-star"></i>
-          <i class="fa fa-solid fa-star-half"></i>
-        </div>
-        <p class="pt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio praesentium delectus porro
-          voluptate ut itaque ducimus vel corporis minus dolores!</p>
-        <div class="flex items-center mt-2">
-          <div class="testi-img">
-            <img class="h-12 w-12 object-cover rounded-full mr-4" src="img/testi3.png" alt="Morgan Steve">
-          </div>
-          <div>
-            <h2 class="font-bold">Martis Bounce</h2>
-            <span>Influencer</span>
-          </div>
-        </div>
       </div>
     </div>
   </section>
