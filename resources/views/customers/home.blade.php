0279c06e35b0eb78593709ae8839bb305132480f
Feek18 <bintangfikri31@gmail.com>
2024-01-26 22:51:03 +0800

    u

--- resources/views/customers/home.blade.php
+++ resources/views/customers/home.blade.php
@@ -88,15 +88,16 @@
   <section
     class="bg-center bg-no-repeat bg-[url('https://images.unsplash.com/photo-1562716190-5c19488ea1fe?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-gray-700 bg-blend-multiply">
     <div class="px-4 mx-auto max-w-screen-xl text-center pt-24 lg:py-56">
-      <!-- <a href="#"
-        class="inline-flex justify-between items-center py-1 px-1 pe-4 mb-7 text-sm text-blue-700 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-200 dark:hover:bg-blue-800"> -->
+      <div
+        class="inline-flex justify-between items-center py-1 px-1 pe-4 mb-7 text-sm text-blue-700 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-200 dark:hover:bg-blue-800">
         <span class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 me-3">New</span> <span
-          class="text-sm font-medium">Jumbotron component was launched! See what's new</span>
+          class="text-sm font-medium">A new car is here for you! See what's new</span>
         <svg class="w-2.5 h-2.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
           viewBox="0 0 6 10">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
             d="m1 9 4-4-4-4" />
         </svg>
+      </div>
       <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Enjoy Your
         Holiday With Awesome Vehicle</h1>
       <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Cruise in Style and Comfort:
