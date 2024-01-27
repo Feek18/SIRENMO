df1b1ad225ecc03a4fc2e38845f61332804cb97e
jalalbinotong <utomorafi207@gmail.com>
2024-01-27 21:54:03 +0800

    fikri

--- resources/views/customers/pages/pesanan_saya.blade.php
+++ resources/views/customers/pages/pesanan_saya.blade.php
@@ -12,12 +12,12 @@
             <table class="items-center w-full mb-0 align-top border-gray-200 text-slate-500">
               <thead class="align-bottom">
                 <tr>
-                  <th
+                  {{-- <th
                     class="px-3 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
                     #</th>
                   <th
                     class="px-3 py-3 pl-2 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                    Kode</th>
+                    Kode</th> --}}
                   <th
                     class="px-3 py-3 pl-2 font-bold text-center uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
                     Tanggal Ambil</th>
@@ -46,7 +46,7 @@
                 @foreach ($pesanans as $pesanan)
                 {{-- @dd($pesanan->kendaraan) --}}
                 <tr>
-                  <td class="px-3 py-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
+                  {{-- <td class="px-3 py-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
                     <div class="flex px-2 py-1">
                       <div class="flex flex-col justify-center">
                         <h6 class="mb-0 text-sm leading-normal">{{ $pesanan->id }}</h6>
@@ -55,7 +55,7 @@
                   </td>
                   <td class="px-3 py-2 align-middle text-center bg-transparent border-b whitespace-nowrap shadow-transparent">
                     <p class="mb-0 text-xs font-semibold text-center leading-tight">{{ $pesanan->kode }}</p>
-                  </td>
+                  </td> --}}
                   <td class="px-3 py-2 align-middle text-center bg-transparent border-b whitespace-nowrap shadow-transparent">
                     <p class="mb-0 text-xs text-center font-semibold leading-tight">{{ $pesanan->tgl_ambil }}</p>
                   </td>
@@ -104,9 +104,6 @@
                     <td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
                         <button type="button" class="text-xs font-semibold leading-tight text-slate-400 ubahPesanan" value="{{ $pesanan->id }}"> Edit </button>
                     </td>
-                    <td class="px-3 py-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-                        <button type="button" class="text-xs font-semibold leading-tight text-slate-400 viewPesanan" value="{{ $pesanan->id }}"> Lihat </button>
-                    </td>
                   @endif
 
                   @if ($pesanan->status == 'terkonfirmasi')
@@ -146,7 +143,7 @@
   </div>
 </div>
 
-{{-- <!-- Modal -->
+<!-- Modal -->
 <div class="modal fade" id="modalPesananUbah" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -155,17 +152,21 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
+        
         <form id="formUbahPesanan" method="post" enctype="multipart/form-data">
+          
           @csrf
           @method('put')
-          <input type="hidden" name="id" id="id-pesanan">
+         
+          <input type="hidden" name="id" id="id-ubah">
+          <input type="hidden" name="kode" id="kode-pesanan-ubah">
             <div class="row">
-              <div class="col">
+              {{-- <div class="col">
                 <div class="mb-3">
                     <label for="kode" class="form-label">Kode</label>
                     <input type="text" class="form-control rounded border-slate-200 rounded border-slate-200" id="kode-pesanan-ubah" name="kode" required>
                 </div>
-              </div>
+              </div> --}}
               <div class="col">
                 <div class="mb-3">
                     <label for="tgl_ambil" class="form-label">Tanggal Ambil</label>
@@ -182,18 +183,8 @@
             <div class="row">
               <div class="col">
                 <div class="mb-3">
-                    <label for="pemesan" class="form-label">Pemesan</label>
-                    <select class="form-select" id="pemesan-ubah" name="customer_id" required>
-                      @foreach ($customers as $cust)
-                        <option value="{{ $cust->id }}">{{ $cust->nama }}</option>
-                      @endforeach
-                    </select>
-                </div>
-              </div>
-              <div class="col">
-                <div class="mb-3">
                     <label for="driver" class="form-label">Pengemudi</label>
-                    <select class="form-select" id="driver-ubah" name="driver_id" required>
+                    <select class="form-select" id="pengemudi-ubah" name="driver_id" required>
                       @foreach ($drivers as $drive)
                         <option value="{{ $drive->id }}">{{ $drive->nama }}</option>
                       @endforeach
@@ -214,12 +205,12 @@
       </div>
       <div class="modal-footer">
           <button type="button" class="block text-white bg-cyan-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" data-bs-dismiss="modal">Close</button>
-          <button type="submit" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 tombol-aksi">Save changes</button>
+          <button type="submit" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save changes</button>
         </form>
       </div>
     </div>
   </div>
-</div> --}}
+</div>
 
 <!-- Card View -->
 <div class="modal fade" id="pesananSaya" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
