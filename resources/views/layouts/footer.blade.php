a8f770d06d41a3778d2ee537ff5715570323ce89
Feek18 <bintangfikri31@gmail.com>
2024-06-22 19:17:18 +0800

    update lp, login, regis, dashboard user

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,33 @@
+<!-- Footer -->
+<footer class="bg-secondary text-white mt-16 py-6">
+    <div class="container mx-auto px-6">
+        <div class="lg:flex lg:justify-between lg:items-center">
+            <div class="lg:w-1/2 mb-6 lg:mb-0">
+                <a href="#" class="text-2xl font-bold flex items-center mb-4 lg:mb-4">
+                    <img class="h-8 mr-2" src="img/logo.png" alt="Logo">
+                    FAST CAR
+                </a>
+                <p class="text-[16px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+                    delectus dolore exercitationem sunt eius quod.</p>
+            </div>
+            <ul class="flex flex-col lg:flex-row lg:items-center lg:space-x-8 mt-6 lg:mt-0 ">
+                <li class="mb-4 lg:mb-0"><a href="#" class="hover:text-color text-sm">About Us</a></li>
+                <li class="mb-4 lg:mb-0"><a href="#" class="hover:text-color text-sm">Our Rents</a></li>
+                <li class="mb-4 lg:mb-0"><a href="#" class="hover:text-color text-sm">Terms &
+                        Conditions</a></li>
+                <li class="mb-4 lg:mb-0"><a href="#" class="hover:text-color text-sm">Privacy Policy</a>
+                </li>
+                <li>
+                    <button class="bg-color p-3">
+                        <a href="#" class="inline-flex items-center"><i
+                                class="fa-solid fa-phone mr-2"></i>+62 822 3788 6327</a>
+                    </button>
+                </li>
+            </ul>
+        </div>
+        <hr class="my-4">
+        <div class="text-[12px] opacity-60 text-center">
+            <span>Copyright ©2023 VOID</span>
+        </div>
+    </div>
+</footer>
